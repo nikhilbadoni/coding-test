ce8de167ad7684602206edbc7861f0e0a54e9d2e
Nikhil Badoni <nikhilbadoni@Nikhils-MacBook-Air.local>
2023-09-29 12:27:07 +0530

    java programs

--- StudentGrades.java
+++ StudentGrades.java
@@ -8,96 +8,87 @@
  * Else “F”
  */
 import java.util.Scanner;
+
 public class StudentGrades {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[]studentNames = new String[10];
-        int[]studentMarks = new int[10];
-
-        // input student name and marks
-        for(int i=0; i<10; i++){
-            System.out.println("Enter name of Student" + (i+1)+ ":");
+        
+        String[] studentNames = new String[10];
+        int[] studentMarks = new int[10];
+        
+        // Input student names and marks
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the name of student " + (i + 1) + ": ");
             studentNames[i] = input.next();
-                        System.out.println("Enter marks of Student" + (i+1)+ ":");
+            System.out.print("Enter the marks of student " + (i + 1) + ": ");
             studentMarks[i] = input.nextInt();
-
         }
-        // Display grades for each student
+        
+        // Display grades for each student using switch-case
         System.out.println("Grades for Students:");
         for (int i = 0; i < 10; i++) {
             String grade;
-            if (studentMarks[i] > 90) {
-                grade = "A";
-            } else if (studentMarks[i] >= 80) {
-                grade = "B";
-            } else if (studentMarks[i] >= 70) {
-                grade = "C";
-            } else if (studentMarks[i] >= 60) {
-                grade = "D";
-            } else if (studentMarks[i] >= 45) {
-                grade = "E";
-            } else {
-                grade = "F";
+            int marks = studentMarks[i];
+            
+            switch (marks / 10) {
+                case 10:
+                case 9:
+                    grade = "A";
+                    break;
+                case 8:
+                    grade = "B";
+                    break;
+                case 7:
+                    grade = "C";
+                    break;
+                case 6:
+                    grade = "D";
+                    break;
+                default:
+                    if (marks >= 45) {
+                        grade = "E";
+                    } else {
+                        grade = "F";
+                    }
+                    break;
             }
             
             System.out.println(studentNames[i] + ": " + grade);
         }
         input.close();
     }
-    
 }
-
 /*
-OUTPUT:- 
- Enter name of Student1:
-Nik
-Enter marks of Student1:
-90
-Enter name of Student2:
-Raj
-Enter marks of Student2:
-88
-Enter name of Student3:
-Viv
-Enter marks of Student3:
-87
-Enter name of Student4:
-Akhi
-Enter marks of Student4:
-89
-Enter name of Student5:
-Div
-Enter marks of Student5:
-78
-Enter name of Student6:
-Sha
-Enter marks of Student6:
-89
-Enter name of Student7:
-ram
-Enter marks of Student7:
-67
-Enter name of Student8:
-Kum
-Enter marks of Student8:
-67
-Enter name of Student9:
-guj
-Enter marks of Student9:
-45
-Enter name of Student10:
-esha
-Enter marks of Student10:
-45
+OUTPUT:-
+ Enter the name of student 1: a
+Enter the marks of student 1: 66
+Enter the name of student 2: b
+Enter the marks of student 2: 67
+Enter the name of student 3: c
+Enter the marks of student 3: 78
+Enter the name of student 4: d
+Enter the marks of student 4: 56
+Enter the name of student 5: w
+Enter the marks of student 5: 456
+Enter the name of student 6: c
+Enter the marks of student 6: 56
+Enter the name of student 7: s
+Enter the marks of student 7: 32
+Enter the name of student 8: x
+Enter the marks of student 8: 45
+Enter the name of student 9: c
+Enter the marks of student 9: 56
+Enter the name of student 10: b
+Enter the marks of student 10: 56
 Grades for Students:
-Nik: B
-Raj: B
-Viv: B
-Akhi: B
-Div: C
-Sha: B
-ram: D
-Kum: D
-guj: E
-esha: E
+a: D
+b: D
+c: C
+d: E
+w: E
+c: E
+s: F
+x: E
+c: E
+b: E
  */
